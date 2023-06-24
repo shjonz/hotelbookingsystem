2100a6f69541946307f6f68891128f4abe6e8047
shjonz <shjonathantan11@gmail.com>
2023-06-24 14:32:02 +0800

    24 june

---
+++ bookingsys/src/views/hotelsList/HotelsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
+
+const HotelsList = () => {
+    return (
+        <div><Navbar/><Header type="list"/></div>
+    )
+}
+
+export default HotelsList
