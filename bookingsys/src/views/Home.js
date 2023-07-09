d737968d4ad817bc79a6d1ce2a4dcc2e050a702a
cord <cordeliatxe@gmail.com>
2023-07-09 17:04:25 +0800

    login register

---
+++ bookingsys/src/views/Home.js
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+
+
+const Home = () => {
+
+
+
+
+  
+//  let navigate = useNavigate()
+  return (
+    <div>
+      <Navbar />
+    <h1>HOME PAGE</h1>
+    </div>
+  );
+}
+
+export default Home;
