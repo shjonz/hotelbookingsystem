3f412e1d1fb6b63d265b4195b9a0632dbca66252
fakinmemes <zacseow@gmail.com>
2023-07-18 12:01:06 +0800

    Minor Changes

--- bookingsys/server/routes/hotels.js
+++ bookingsys/server/routes/hotels.js
@@ -105,7 +105,7 @@ async function hotelListPrices(req, res, next) {
         };
     });
 
-    res.hotelListPrices = hotelListPrices;
+    res.hotelListPrices = hotelListPrices.filter(hotel => hotel.price !== undefined);;
     next();
 }
 
