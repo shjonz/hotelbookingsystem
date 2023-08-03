8e6c7a08335bf224b396018e41aee43f67397297
shjonz <shjonathantan11@gmail.com>
2023-08-03 23:01:00 +0800

    3 aug 2023 11 pm update

--- bookingsys/client/src/components/featured/Featured.js
+++ bookingsys/client/src/components/featured/Featured.js
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import "./featured.css";
 import nyimg from '../images/newyork.jpg';
+import axios from 'axios';
+import "../navbar/Navbar.js";
+import { SelectedOptionContext } from '../../context/SelectedOptionContext';
+
 
 const Featured = () => {
+    const [country, setCountry] = useState([]);
+    const { selectedOption, setSelectedOption } = useContext(SelectedOptionContext);
+    console.log(' inside featured, ', selectedOption);
+
+    // useEffect( () => {
+        
+    //       //const { data } = await axios.get("http://localhost:3000/api/hotels/default?destination_id=WD0M");
+    //       //const asyncFn = async () => { .... };
+    //         //asyncFn();
+    //     //const { data } = await axios.get(`http://localhost:3000/search/?name=${selectedOption}`);
+    //       //const { data } = await axios.get(`http://localhost:3000/search/?name=Singapore`);
+
+    //     setCountry(data.slice(0, 3));
+    //   }, [selectedOption]);
+
+    useEffect(() => {
+        async function defaultCountry() {
+            console.log('inside useeffect selectedoption ', selectedOption.label);
+            const { data } = await axios.get(`http://localhost:3000/search/?name=${selectedOption.label}`);
+            //const { data } = await axios.get(`http://localhost:3000/search/?name=Singapore`);
+            setCountry(data.slice(0,3));
+        }
+        // Trigger the fetch
+        defaultCountry();
+      }, [selectedOption]);
+    
+
     return (
         <div className='featured'>
             
+            {country && country.slice(0, 3).map((item, index) => (
             <div className='featuredItem'>
                 <img className='featuredImg' src={nyimg} alt="newyork"></img>
 
                 <div className='featuredTitles'>
-                    <h1>Dublin</h1>
-                    <h2>123 properties</h2>
+                    <h1>{item.name}</h1>
+                    <h2>{item.uid}</h2>
                 </div>
 
             </div>
+            ))}
 
 
 
-            <div className='featuredItem'>
+            {/* <div className='featuredItem'>
                 <img className='featuredImg' src={nyimg}></img>
 
                 <div className='featuredTitles'>
@@ -38,7 +71,7 @@ const Featured = () => {
                     <h2>533 properties</h2>
                 </div>
 
-            </div>
+            </div> */}
 
         </div>
     )
--- bookingsys/client/src/components/navbar/Navbar.js
+++ bookingsys/client/src/components/navbar/Navbar.js
@@ -1,11 +1,15 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import "./navbar.css"
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import data from '../../countrycodeflagname.json';
+import Dropdown from "react-dropdown";
+import { SelectedOptionContext } from "../../context/SelectedOptionContext";
 
 const Navbar = () => {
     const { user, dispatch } = useContext(AuthContext);
     const navigate = useNavigate();
+    const { selectedOption, setSelectedOption } = useContext(SelectedOptionContext); // Use the context here
     console.log(' navbar lemme see user info ', user);
     const handleClick = () => {
         window.localStorage.clear()
@@ -13,9 +17,10 @@ const Navbar = () => {
         console.log( 'press logout handleclick check user info ', user );
         navigate("/");
     }
-    // const viewProfile = () => {
-    //     navigate("profile");
-    // }
+
+    //console.log( ' selected option change ', selectedOption);
+    
+    
     
     if (user) {
         return (
@@ -29,6 +34,33 @@ const Navbar = () => {
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#team">Currency</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/profile">{user}</a></li>
                         {/* <button onClick={viewProfile} className="showusername">{user}</button> */}
+                        <Dropdown
+                            //placeholder="Singapore"
+                            className="dropdown"
+                            options={data.map((item) => ({ value: item.code, label: item.name, flag: item.flag }))}
+        // options={countries.map((item) => ({ value: item.code, label: item.name, flag: item.flag }))}
+        // optionRenderer={(option) => <CustomOption option={option} />} // Use the custom component
+                            values={[]} // Provide an empty array or the initial selected values (if any)
+                            multi={false} // Set this to true for a multi-select dropdown
+                            onChange={(values) => 
+                                setSelectedOption(values) } // This function will be called when the selection changes
+                            labelField="label" // Set the field to be displayed as the label in the dropdown
+                            valueField="value"// Set the field to be used as the actual value when an option is selected
+        //take labelfield for Featured.js to look for country
+                            color="primary" // Customize the color of the dropdown
+                            keepSelectedInList={true} // Keep selected options in the dropdown list
+                            dropdownGap={5} // Set the gap between the dropdown and the select box
+        // value="Country"
+        // value="Singapore"
+                            isSearchable={true}
+                            value={selectedOption} // Use the selectedOption from context as the value
+                            onSelect={(value) => setSelectedOption(value)} // Update the selectedOption when a new option is selected
+        // onChange={(value) => console.log('change!', value)}
+        // onSelect={(value) => console.log('selected!', value)} // always fires once a selection happens even if there is no change
+                            onClose={(closedBySelection) => console.log('closedBySelection?:', closedBySelection)}
+                            onOpen={() => console.log('open!')}>
+                        </Dropdown> 
+
                         <button onClick={handleClick} className="navButton"><Link>Logout</Link></button>       
                     </div> 
                 </div>
@@ -39,6 +71,31 @@ const Navbar = () => {
         <div className="navbar">
             <div className="navbarContainer">
                 <span className="logo">bookingwebsite</span>
+                <div><Dropdown
+                    //placeholder="Singapore"
+                    className="dropdown"
+                    options={data.map((item) => ({ value: item.code, label: item.name, flag: item.flag }))}
+        // options={countries.map((item) => ({ value: item.code, label: item.name, flag: item.flag }))}
+        // optionRenderer={(option) => <CustomOption option={option} />} // Use the custom component
+                    values={[]} // Provide an empty array or the initial selected values (if any)
+                    multi={false} // Set this to true for a multi-select dropdown
+                    onChange={(values) => setSelectedOption(values)} // This function will be called when the selection changes
+    //take labelfield for Featured.js to look for country
+                    labelField="label" // Set the field to be displayed as the label in the dropdown
+                    valueField="value"// Set the field to be used as the actual value when an option is selected
+        
+                    color="primary" // Customize the color of the dropdown
+                    keepSelectedInList={true} // Keep selected options in the dropdown list
+                    dropdownGap={5} // Set the gap between the dropdown and the select box
+        // value="Singapore"
+                    isSearchable={true}
+                    value={selectedOption} // Use the selectedOption from context as the value
+                    onSelect={(value) => setSelectedOption(value)} // Update the selectedOption when a new option is selected
+        // onChange={(value) => console.log('change!', value)}
+        // onSelect={(value) => console.log('selected!', value)} // always fires once a selection happens even if there is no change
+                    onClose={(closedBySelection) => console.log('closedBySelection?:', closedBySelection)}
+                    onOpen={() => console.log('open!')}>
+                </Dropdown></div> 
                 {user ? user : (
                 <div className="navItems">
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Cities</a></li>
